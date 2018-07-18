5760031fa5c3a779d3a536f83ce8143171200755
John O'Neill <j.oneill@kainos.com>
2018-07-18 16:25:13 +0100

    FIX: Added condition to if to check for state exists

--- app/middleware/submissionMiddleware.js
+++ app/middleware/submissionMiddleware.js
@@ -3,9 +3,24 @@ const config = require('config');
 const hasSubmitted = function(req, res, next) {
   const session = req.session;
 
+  /*
   const hasSubmittedEnabled = ['prod'].includes(config.deployment_env);
 
-  if (hasSubmittedEnabled && !this.enabledAfterSubmission && session.caseId) { // eslint-disable-line
+  if (hasSubmittedEnabled && !this.enabledAfterSubmission && session.caseId && session.state) { // eslint-disable-line
+    switch (session.state) {
+    case 'AwaitingPayment':
+      return res.redirect('/application-submitted');
+    case 'Rejected':
+      return next();
+    default:
+      return res.redirect('/application-submitted-awaiting-response');
+    }
+  }
+  return next();
+};
+*/
+
+if (!this.enabledAfterSubmission && session.caseId && session.state) { // eslint-disable-line
     switch (session.state) {
     case 'AwaitingPayment':
       return res.redirect('/application-submitted');
